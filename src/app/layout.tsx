80a81b96fd906709535cfdfb2d3182ddf378c6a4
Davi Oliveira Brito <davioliveirabrito07@gmail.com>
2026-03-20 14:53:09 -0300

    ajuste na Layout.tsx para deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,7 @@ import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }){
   return (
     <html lang="pt-BR">
       <body className={inter.className}>
